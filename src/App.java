d8daadcb534f382ad46771cb6bca4eaf56e4f745
julianasantiago100 <julianafpsc@gmail.com>
2024-04-18 00:05:01 -0300

    test: montando um grafo e visualizando a matrizAdj

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import grafo_direcionado.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Grafo g = new Grafo<>(7, 21);
+        var v = g.AddVertice("Estudar Grafos", 1, 2);
+        var f = g.AddVertice("Estudar Rust", 1, 2);
+        g.AddAresta(v, f, 10);
+        g.AddAresta(f, v, 10);
+        var j = g.AddVertice("Aula Rust", 0, 2);
+        g.AddAresta(v, j, 2);
+        g.AddAresta(j, v, 2);
+        g.AddAresta(f, j, 2);
+        g.AddAresta(j, f, 2);
+        g.GerarMatrizDeAdj();
     }
 }
