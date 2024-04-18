290efa31400ca06e64f3025aa9635ae642f78c58
julianasantiago100 <julianafpsc@gmail.com>
2024-04-18 00:26:57 -0300

    fix: updating App.java

--- src/App.java
+++ src/App.java
@@ -3,15 +3,14 @@ import grafo_direcionado.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Grafo g = new Grafo<>(7, 21);
-        var v = g.AddVertice("Estudar Grafos", 1, 2);
-        var f = g.AddVertice("Estudar Rust", 1, 2);
-        g.AddAresta(v, f);
-        g.AddAresta(f, v);
-        var j = g.AddVertice("Aula Rust", 0, 2);
-        g.AddAresta(v, j);
-        g.AddAresta(j, v);
-        g.AddAresta(f, j);
-        g.AddAresta(j, f);
+        g.AddVertice("Estudar Grafos", 1, 2);
+        g.AddVertice("Estudar Rust", 1, 2);
+        g.AddVertice("Aula Rust", 0, 2);
+        g.AddVertice("Aula de Natação", 0, 3);
+
+        // !Este método gera um grafo completo a partir dos vértices inseridos no grafo
+        g.gerarGrafoCompleto();
+
         g.GerarMatrizDeAdj();
     }
 }
